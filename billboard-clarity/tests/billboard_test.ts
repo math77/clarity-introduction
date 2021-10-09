1f6ec59d89f66b8ae85ef357c282e0cb3dba7717
Matheus Emanuel <matheusemanuel745@gmail.com>
2021-10-09 15:04:00 -0300

    first commit

---
+++ billboard-clarity/tests/billboard_test.ts
@@ -0,0 +1,36 @@
+
+import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.12.0/index.ts';
+import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
+
+Clarinet.test({
+  name: 'A quick demo on how to assert expectations',
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let wallet_1 = accounts.get('wallet_1')!;
+
+    let assetMaps = chain.getAssetsMaps();
+    const balance = assetMaps.assets['STX'][wallet_1.address];
+
+    let block = chain.mineBlock([
+      Tx.contractCall('billboard', 'set-message', [types.utf8('testing')], wallet_1.address),
+      Tx.contractCall('billboard', 'get-message', [], wallet_1.address),
+      Tx.contractCall('billboard', 'set-message', [types.utf8('testing...')], wallet_1.address),
+      Tx.contractCall('billboard', 'get-message', [], wallet_1.address),
+    ]);
+
+    assertEquals(block.receipts.length, 4);
+    assertEquals(block.height, 2);
+
+    block.receipts[1].result.expectUtf8('testing');
+
+    block.receipts[3].result.expectUtf8('testing...');
+
+    let [event] = block.receipts[0].events;
+    let { sender, recipient, amount } = event.stx_transfer_event;
+    sender.expectPrincipal('ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5');
+    recipient.expectPrincipal('ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.billboard');
+    amount.expectInt(100);
+
+    assetMaps = chain.getAssetsMaps();
+    assertEquals(assetMaps.assets['STX'][wallet_1.address], balance - 210);
+  },
+});
